fd19165e197b413b5a5ab986579ec7026ab7f031
Dantehr-13 <a01668070@tec.mx>
2026-04-19 23:55:48 -0600

    error de contraseña

--- Proyecto con Base de Datos/util/database.js
+++ Proyecto con Base de Datos/util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'lol',
-    password: ''
+    password: 'dantesaur10'
 });
 
 module.exports = pool.promise();
